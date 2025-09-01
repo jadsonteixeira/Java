d49e40dddc02c09f89c5758d1400cf53f26ae7d2
Jadson Teixeira <jadsontxm@gmail.com>
2025-08-31 22:37:17 -0300

    Criação de Classes

---
+++ poo/loiane/aula24Classes/Carro.java
@@ -0,0 +1,9 @@
+package poo.loiane.aula24Classes;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
