8fbf08e5d322db8f25033d267245b24bd6d47950
Jadson Teixeira <jadsontxm@gmail.com>
2025-09-09 22:05:01 -0300

    Construtores

---
+++ nelioalves/section9/entities/Product.java
@@ -0,0 +1,37 @@
+package nelioalves.section9.entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    // construtor
+    public Product(String name, double price, int quantity) {
+        this.name = name; // this significa uma referência ao objeto
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // retornar o valor total em estoque
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    // adicionar a quantidade de produtos
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    // remover do estoque
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ " + String.format("%.2f", price)
+                + ", " + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
