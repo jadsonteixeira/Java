26d32c08094a610d463ee6124dd9987944e948e8
Jadson Teixeira <jadsontxm@gmail.com>
2025-09-12 13:16:38 -0300

    Encapsulamento

---
+++ guanabara/aula06Encapsulamento/Aula06.java
@@ -0,0 +1,11 @@
+package guanabara.aula06Encapsulamento;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c =new ControleRemoto();
+        c.maisVolume();
+        c.ligarMudo();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
